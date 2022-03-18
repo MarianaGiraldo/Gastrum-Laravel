a34666ff7181c3ca5bb3cb13fcbb62305a1a950b
MarianaGiraldo <mgiraldo594@misena.edu.co>
2022-03-18 09:18:24 -0500

    Project init

---
+++ public/js/app.js
@@ -0,0 +1,6 @@
+require('./bootstrap');
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, []);
+});
